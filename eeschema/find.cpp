58c9b3a2405a90ca2ef28c151b66d01ffef2a2c8
Seth Hillbrand <hillbrand@ucdavis.edu>
2017-12-09 20:34:36 -0800

    Eeschema: TestDanglingEnds on Cross-probe
    Fixes: lp:1672381
    * https://bugs.launchpad.net/kicad/+bug/1672381

--- eeschema/find.cpp
+++ eeschema/find.cpp
@@ -193,6 +193,7 @@ SCH_ITEM* SCH_EDIT_FRAME::FindComponentAndItem( const wxString& aReference,
             sheet->LastScreen()->SetZoom( GetScreen()->GetZoom() );
             *m_CurrentSheet = *sheet;
             m_CurrentSheet->UpdateAllScreenReferences();
+            sheet->LastScreen()->TestDanglingEnds();
             centerAndRedraw = true;
         }
 
